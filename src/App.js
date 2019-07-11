61ca8c8000b05939e07c0f4a838555b424b3a8d4
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2019-07-11 12:04:00 +0300

    Baseline Aplication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Machinery and equipment distributor.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.europages.co.uk/companies/Russia/Moscow/Distributor/machinery%20and%20equipment.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Home Page
         </a>
       </header>
     </div>
