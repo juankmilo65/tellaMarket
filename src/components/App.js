d2318fa787edab0dbe089f7f93ba1b566130e5ba
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2019-08-02 12:14:29 +0300

    Se ajusta renderización de la pag justo cuando esté todo cargado en el dom.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { firebaseConnect } from "react-redux-firebase";
+import { compose } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import Dashboard from "./dashboard/Dashboard";
@@ -10,21 +12,32 @@ import CreateProject from "./projects/CreateProject";
 
 class App extends Component {
   render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/project/:id" component={ProjectDetail} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/createproject" component={CreateProject} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
+    const { auth } = this.props;
+    if (auth.isLoaded) {
+      return (
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/project/:id" component={ProjectDetail} />
+              <Route path="/signin" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/createproject" component={CreateProject} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      );
+    }
+    return null;
   }
 }
 
-export default connect(state => state)(App);
+const mapStateToProps = state => ({
+  auth: state.firebase.auth
+});
+
+export default compose(
+  firebaseConnect(),
+  connect(mapStateToProps)
+)(App);
