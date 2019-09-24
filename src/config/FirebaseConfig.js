f0797ae9bebee9df2421226ac62543bfb124ccbe
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2019-09-24 20:32:32 +0300

    no message

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -3,15 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
 
-const config = {
-  apiKey: "AIzaSyCQjxOeig-6bzMW1g1yMXarKpoOYzJaqmw",
-  authDomain: "tellamachines.firebaseapp.com",
-  databaseURL: "https://tellamachines.firebaseio.com",
-  projectId: "tellamachines",
-  storageBucket: "tellamachines.appspot.com",
-  messagingSenderId: "762949691937",
-  appId: "1:762949691937:web:043b23663160a576"
-};
+const config = {};
 
 firebase.initializeApp(config);
 const storage = firebase.storage();
