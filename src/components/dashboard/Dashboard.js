9ef9fa37184f74a22fffbd7657764de13f3140c6
yeison del rio <delriovargas@gmail.com>
2019-10-16 01:09:29 -0500

    Cambiso de seccion proveedores

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -14,6 +14,9 @@ import bannerXs2 from "../commons/carousel/img/banner-xs-2.png";
 import bannerXs3 from "../commons/carousel/img/banner-xs-3.png";
 import bannerXs4 from "../commons/carousel/img/banner-xs-4.png";
 import logoWhite from "../commons/carousel/img/logo-white.png";
+import imgproveedor from "../commons/carousel/img/imgprovedor.png";
+import producto1 from "../commons/carousel/img/producto1.png";
+import producto2 from "../commons/carousel/img/producto2.png";
 import "./dashboard.scss";
 
 class Dashboard extends Component {
@@ -173,6 +176,54 @@ class Dashboard extends Component {
                 Nuevos <span>proveedores</span>
               </label>
             </div>
+            <div className="content-provider">
+                <div className="box-provider--big">
+                  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block" src={imgproveedor} alt="First slide"/>
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block" src={imgproveedor} alt="Second slide"/>
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block" src={imgproveedor} alt="Third slide"/>
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+                </div>
+                <div className="box-provider--double">
+                  <div className="item-box--provider">
+                    <div className="item-box--text">
+                      <label className="title-item--provider">Suspendisse dignissim tristique </label>
+                      <span className="description-item--provider">Fusce vel nibh sollicitudin</span>
+                      <button className="btns btn-se">cotizar</button>
+                    </div>
+                    <div className="item-box--img">
+                      <img class="d-block" src={producto1} alt=""/>
+                    </div>
+                  </div>
+                  <div className="item-box--provider">
+                    <div className="item-box--text">
+                      <label className="title-item--provider">Integer vehicula |pellentesque</label>
+                      <span className="description-item--provider">Integer ac metus eu sapien placerat</span>
+                      <button className="btns btn-se">cotizar</button>
+                    </div>
+                    <div className="item-box--img">
+                      <img class="d-block" src={producto2} alt=""/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+           
           </div>
         </div>
         <div>
