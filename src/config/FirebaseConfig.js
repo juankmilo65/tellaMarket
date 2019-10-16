8889d54c469df089ff1b418d10c2754728b72564
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2019-10-17 02:37:04 +0300

    .

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -4,13 +4,22 @@ import "firebase/auth";
 import "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyCQjxOeig-6bzMW1g1yMXarKpoOYzJaqmw",
-  authDomain: "tellamachines.firebaseapp.com",
-  databaseURL: "https://tellamachines.firebaseio.com",
-  projectId: "tellamachines",
-  storageBucket: "tellamachines.appspot.com",
-  messagingSenderId: "762949691937",
-  appId: "1:762949691937:web:043b23663160a576"
+  // apiKey: "AIzaSyCQjxOeig-6bzMW1g1yMXarKpoOYzJaqmw",
+  // authDomain: "tellamachines.firebaseapp.com",
+  // databaseURL: "https://tellamachines.firebaseio.com",
+  // projectId: "tellamachines",
+  // storageBucket: "tellamachines.appspot.com",
+  // messagingSenderId: "762949691937",
+  // appId: "1:762949691937:web:043b23663160a576"
+
+  apiKey: "AIzaSyDPv44r04rFaOFqRjoKNiyP65jh5RDt_qk",
+  authDomain: "tellamarkettest.firebaseapp.com",
+  databaseURL: "https://tellamarkettest.firebaseio.com",
+  projectId: "tellamarkettest",
+  storageBucket: "tellamarkettest.appspot.com",
+  messagingSenderId: "878870201945",
+  appId: "1:878870201945:web:415fdbe5688f511b691348",
+  measurementId: "G-5N93GRXBGV"
 };
 
 firebase.initializeApp(config);
