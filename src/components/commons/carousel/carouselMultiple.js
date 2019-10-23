35def2957e6095de48e9bf7bef4e9bbc871ea15b
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2019-10-23 13:34:30 +0300

    se realiza merge

--- src/components/commons/carousel/carouselMultiple.js
+++ src/components/commons/carousel/carouselMultiple.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 import CarouselImage from "../carousel/carouselImage";
+import { Redirect } from "react-router-dom";
 import "./carousel.scss";
 
 class carouselMultiple extends Component {
+  state = {
+    redirect: false
+  };
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    });
+  };
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/itemDetail",
+            state: { id: "123" }
+          }}
+        />
+      );
+    }
+  };
   render() {
     const { items } = this.props;
     return (
@@ -18,6 +41,13 @@ class carouselMultiple extends Component {
                       <div className="title-product">{item.titleproduct}</div>
                       <div className="title-category">{item.titlecategory}</div>
                       <div className="value--price">{item.valueprice}</div>
+
+                      <button
+                        className="btns btn-go"
+                        onClick={this.setRedirect}
+                      >
+                        Ver mas
+                      </button>
                     </div>
                   </div>
                 </div>
